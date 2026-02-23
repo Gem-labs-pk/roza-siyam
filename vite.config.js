a284976a46a43187ee4ac2e8784160d60069b98c
Muhammad Saqib Zahid <m.saqibzahid.iqbal@gmail.com>
2026-02-23 16:04:40 +0500

    Setting up for Github Pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/roza-siyam/", // MAKE SURE THIS MATCHES YOUR EXACT GITHUB REPOSITORY NAME
 })
